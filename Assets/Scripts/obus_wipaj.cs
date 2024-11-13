ce55e05d99cab4a027a77fcf23b56d757bb1b055
alj <alexisjust@gmail.com>
2024-11-13 16:54:56 +0100

    preparation in code for kaboom and tweaked some stuff

--- Assets/Scripts/obus_wipaj.cs
+++ Assets/Scripts/obus_wipaj.cs
@@ -5,13 +5,27 @@ using UnityEngine;
 public class obus_wipaj : MonoBehaviour
 {
     [SerializeField] private float altitude = -5.0f;
+    private int layer_ground;
 
+
+    void Start()
+    {
+        // Initialize the ground layer once, for example by getting the layer by name
+        layer_ground = LayerMask.NameToLayer("Ground");
+    }
 
     // Update is called once per frame
     private void Update()
     {
         if (transform.position.y < altitude)
         {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.layer == layer_ground){
             Destroy(gameObject);
         }
     }
