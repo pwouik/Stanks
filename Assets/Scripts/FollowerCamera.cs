f8d61b5935be7f358e48725fea3e57619d0f15be
pwouik <pwouick@gmail.com>
2024-11-25 15:22:45 +0100

    camera

--- Assets/Scripts/FollowerCamera.cs
+++ Assets/Scripts/FollowerCamera.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,6 +9,7 @@ public class FollowerCamera : MonoBehaviour
     [SerializeField] private float minHeight = 6f, maxHeight = 64f;
     [SerializeField] private float zoomFactor = .7f;
     [SerializeField] private float offsetY = 6f;
+    [SerializeField] private float offsetZ = -15f;
 
     void Update()
     {
@@ -15,8 +17,9 @@ public class FollowerCamera : MonoBehaviour
             return;
 
         Vector3 midpoint = (leftPlayerTank.transform.position + rightPlayerTank.transform.position) / 2f;
+        midpoint.y = Mathf.Max(leftPlayerTank.transform.position.y, rightPlayerTank.transform.position.y);
         float distanceBetweenTanks = Vector3.Distance(leftPlayerTank.transform.position, rightPlayerTank.transform.position);
         float adjustedY = Mathf.Clamp(distanceBetweenTanks * zoomFactor + offsetY, minHeight, maxHeight);
-        transform.position = new Vector3(midpoint.x, adjustedY, midpoint.z);
+        transform.position = new Vector3(midpoint.x, adjustedY + midpoint.y, midpoint.z+offsetZ);
     }
 }
