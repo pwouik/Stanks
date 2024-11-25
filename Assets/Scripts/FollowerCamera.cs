1d7ed992f04a025bbdf394bb0f860b80e6d3436b
bolamigo <27642023+bolamigo@users.noreply.github.com>
2024-11-25 13:58:42 +0100

    [fix] follower camera keeps tanks from being hidden by buttons

--- Assets/Scripts/FollowerCamera.cs
+++ Assets/Scripts/FollowerCamera.cs
@@ -5,18 +5,18 @@ using UnityEngine;
 public class FollowerCamera : MonoBehaviour
 {
     [SerializeField] private GameObject leftPlayerTank, rightPlayerTank;
-    [SerializeField] private float minHeight = 4f, maxHeight = 35f;
-    [SerializeField] private float zoomFactor = 1f;
-    [SerializeField] private float offsetY = 2f;
+    [SerializeField] private float minHeight = 6f, maxHeight = 64f;
+    [SerializeField] private float zoomFactor = .7f;
+    [SerializeField] private float offsetY = 6f;
 
     void Update()
     {
-        if (leftPlayerTank != null && rightPlayerTank != null)
-        {
-            Vector3 midpoint = (leftPlayerTank.transform.position + rightPlayerTank.transform.position) / 2f;
-            float distanceBetweenTanks = Vector3.Distance(leftPlayerTank.transform.position, rightPlayerTank.transform.position);
-            float adjustedY = Mathf.Clamp(distanceBetweenTanks * zoomFactor + offsetY, minHeight, maxHeight);
-            transform.position = new Vector3(midpoint.x, adjustedY, midpoint.z);
-        }
+        if (leftPlayerTank == null || rightPlayerTank == null)
+            return;
+
+        Vector3 midpoint = (leftPlayerTank.transform.position + rightPlayerTank.transform.position) / 2f;
+        float distanceBetweenTanks = Vector3.Distance(leftPlayerTank.transform.position, rightPlayerTank.transform.position);
+        float adjustedY = Mathf.Clamp(distanceBetweenTanks * zoomFactor + offsetY, minHeight, maxHeight);
+        transform.position = new Vector3(midpoint.x, adjustedY, midpoint.z);
     }
 }
