d0370ff8800009d266bb2e47254bf2399e5ff2b5
alj <alexisjust@gmail.com>
2024-11-25 12:13:40 +0100

    fixed firing sequence
    the angle at which obus were fired was fixed

--- Assets/Scripts/game_wipaj.cs
+++ Assets/Scripts/game_wipaj.cs
@@ -25,7 +25,7 @@ public class game_wipaj : MonoBehaviour
     [SerializeField] private float obus_speed = 10;
     [SerializeField] private float fire_height = 1f;
     //temporary for debug and should be removed later
-    [SerializeField] private float fire_angle = 60;
+    [SerializeField] private float fire_angle = 200;
     [SerializeField] private float explosionRadius = 6;
     [SerializeField] private float explosionStrenght = 2500;
 
@@ -42,7 +42,7 @@ public class game_wipaj : MonoBehaviour
 
     void handleInputs()
     {
-        //movement p1
+        //movement p1 (deprecated)
         if (Input.GetKeyDown(dKey1)) //TODO replace with pressed on mov joystick
         { 
             //todo remove p1_direction and compute a (normalized ? may don't normalized it) direction from joyssick
@@ -50,7 +50,7 @@ public class game_wipaj : MonoBehaviour
             rgb_p1.AddForce(force * player_speed, ForceMode.Impulse); //TODO ne pas utiliser impulse
         }
 
-        //movement p2
+        //movement p2 (deprecated)
         if (Input.GetKeyDown(dKey2)) //TODO replace with pressed on mov joystick
         { 
             //todo remove p2_direction and compute a (normalized ? may don't normalized it) direction from joyssick
@@ -67,7 +67,7 @@ public class game_wipaj : MonoBehaviour
             float rot = player1.transform.rotation.eulerAngles.y * Mathf.Deg2Rad;
             obus.transform.position = player1.transform.position + new Vector3(0,1,0);
             obus.transform.rotation = player1.transform.rotation;
-            obus.transform.Rotate(new Vector3(-fire_angle, 0, 0)); //TODO fix rotation value depending on global varibale fire_height
+            obus.transform.Rotate(new Vector3(fire_angle, 0, 0)); //TODO fix rotation value depending on global varibale fire_height
 
             //forward explode parent to obus prefab
             obus_wipaj obusScript = obus.GetComponent<obus_wipaj>();
