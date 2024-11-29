884f9e1a9ca021c97df6ac9c611400f66b592f62
pwouik <pwouick@gmail.com>
2024-11-29 16:56:50 +0100

    stuffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff

--- Assets/Scripts/TankController.cs
+++ Assets/Scripts/TankController.cs
@@ -9,14 +9,24 @@ public class TankController : MonoBehaviour
     public void SetDir(Vector2 d){
         dir = d * player_speed;
     }
-    // Update is called once per frame
+    private bool IsGrounded()
+    {
+        return Physics.Raycast(transform.position, -Vector3.up, 9f);
+    }
     void FixedUpdate()
     {
-        Rigidbody brb = GetComponent<Rigidbody>();
-        brb.velocity = new Vector3(dir.x,brb.velocity.y,dir.y);
+        if(IsGrounded()){
+            Rigidbody brb = GetComponent<Rigidbody>();
+            var dist = new Vector3(dir.x,brb.velocity.y,dir.y) - brb.velocity;
+            brb.velocity += dist/Math.Max(1.0f,dist.magnitude);
+        }
     }
     void Update(){
         if(dir.magnitude>0.001)
             transform.rotation = Quaternion.Euler(0.0f,(float)Math.Atan2(dir.x,dir.y)*180.0f/3.1415926f+180.0f,0.0f);
+            Rigidbody brb = GetComponent<Rigidbody>();
+            brb.angularVelocity = new();
+
     }
+    
 }
